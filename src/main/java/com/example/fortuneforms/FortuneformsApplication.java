1b88c1528c3a146d3a8595c33b7c57d0f72e3ea4
yapiko <bloomingjewels7021@gmail.com>
2022-07-23 08:22:19 +0900

    first commit

---
+++ src/main/java/com/example/fortuneforms/FortuneformsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fortuneforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneformsApplication.class, args);
+	}
+
+}
