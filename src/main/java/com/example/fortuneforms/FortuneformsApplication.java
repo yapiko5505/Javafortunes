ea3a4c47ee68123179e257c2f2320e0005e17579
yapiko <bloomingjewels7021@gmail.com>
2022-07-23 08:53:39 +0900

    java3

--- src/main/java/com/example/fortuneforms/FortuneformsApplication.java
+++ src/main/java/com/example/fortuneforms/FortuneformsApplication.java
@@ -2,8 +2,11 @@ package com.example.fortuneforms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = Fortune.class)
 public class FortuneformsApplication {
 
 	public static void main(String[] args) {
